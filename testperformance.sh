89a7d32db3abcafedd3d8f74249f067661e6eb35
Ted Bowman <ted.git@tedbow.com>
2016-04-08 05:35:00 +0000

    re-install and regenerate entities before Multiversion install. Memormy limit reached. 512MB

--- testperformance.sh
+++ testperformance.sh
@@ -4,7 +4,7 @@ SITE_ALIAS='d8wps.dev'
 DRUSH="drush @${SITE_ALIAS}"
 DCONSOLE="drupal --target=${SITE_ALIAS}"
 ENTITY_CNT=10000
-REVISIONS_CNT=2
+REVISIONS_CNT=200
 dt=`date '+%Y_%m_%d-%H_%M_%S'`
 
 $DRUSH si -y --account-pass=pass
@@ -25,7 +25,17 @@ casperjs loadtest.js --xhprof=1 --login=1
 ##$DCONSOLE xhprof_csv:summary with_mv.csv
 
 #$DRUSH cr
-$DRUSH en multiversion -y
+
+$DRUSH si -y --account-pass=pass
+$DCONSOLE module:install performance_tester -y
+$DCONSOLE module:install multiversion -y
+#$DRUSH en xhprof_csv -y
+$DCONSOLE module:install devel devel_generate -y
+#$DCONSOLE xhprof_csv:summary with_mv.csv
+$DRUSH genu $ENTITY_CNT
+$DRUSH gent tags $ENTITY_CNT
+$DRUSH genc $ENTITY_CNT --types=page
+
 $DCONSOLE performance_tester:revisions node --count $REVISIONS_CNT
 $DCONSOLE performance_tester:revisions taxonomy_term --count $REVISIONS_CNT
 $DCONSOLE performance_tester:revisions user --count $REVISIONS_CNT
